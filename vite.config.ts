305ca706c030ddcb3143a55a16125f62a0fbc4b9
kuangxu <kuangxu@gmail.com>
2025-10-01 15:22:35 -0700

    Fix base path and add CNAME for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/kuang_homesite/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
