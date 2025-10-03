c67883419a54a278d988d27ae95ef43582046735
kuangxu <kuangxu@gmail.com>
2025-10-03 09:12:16 -0700

    updated routing type error

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ const App = () => {
         <Toaster />
         <Sonner />
         <HashRouter>
-          <RouteHandler />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/research" element={<Research />} />
